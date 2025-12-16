944dd1fbb94f0a587d7dfedc5558423765e325c7
Alexandru Ban <alexandru.ban@nestle.com>
2025-12-16 20:00:16 +0100

    Fix integration tests setup

---
+++ Tests/Carquitecture.IntegrationTests/Application/Features/Vehicles/CreateVehiclesTests.cs
@@ -0,0 +1,44 @@
+﻿using Carquitecture.API.Controllers.Vehicle.Requests;
+using Carquitecture.Application.Features.Vehicles.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+
+namespace Carquitecture.IntegrationTests.Application.Features.Vehicles;
+
+// Revisar opciones a parte de IClassFixture y para qué sirve cada una.
+public class CreateVehiclesTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _client;
+    private readonly CustomWebApplicationFactory _factory;
+    public CreateVehiclesTests(CustomWebApplicationFactory factory)
+    {
+        _factory = factory;
+        _client = factory.CreateClient(new WebApplicationFactoryClientOptions 
+        {
+            AllowAutoRedirect = false
+        });
+    }
+    [Fact]
+    public async Task CreateVehicle_ReturnsSuccessStatusCode()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var newVehicle = new CreateVehicleRequest
+        {
+            LicensePlate = new LicensePlateDto("ABC123", "ES"),
+            Type = "Renault",
+            Owners =
+            [
+                new("Test", "User", true)
+            ],
+            Seats =
+            [
+                new CreateSeatDto("Leather", "Black")
+            ]
+        };
+        // Act
+        var response = await client.PostAsJsonAsync("/api/vehicles", newVehicle);
+        // Assert
+        response.EnsureSuccessStatusCode();
+    }
+}
---
+++ Tests/Carquitecture.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Carquitecture.IntegrationTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        // Revisar para cargar el appsettings de test.
+        builder.ConfigureServices(services =>
+        {});
+
+        builder.UseEnvironment("Test");
+    }
+}
